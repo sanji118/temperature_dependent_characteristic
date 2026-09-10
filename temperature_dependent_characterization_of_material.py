7981ec4e1ba59abd37ec0f28ea6c3c282860019a
sanji118 <sanjishifa32@gmail.com>
2026-09-10 11:32:33 +0600

    plotted the graph

--- temperature_dependent_characterization_of_material.py
+++ temperature_dependent_characterization_of_material.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 "CSV file"
-data=pd.read_csv("C:\programming_course_1\python-course\python course\project-for-jannat-sir/temperature_resistance.csv")
+data=pd.read_csv("C:/programming_course_1/python-course/temperature_dependent_characteristic/temperature_resistance.csv")
 
 "Extract data"
 T=data["Temperature_C"]
@@ -13,8 +13,8 @@ print(data)
 
 "Plot"
 plt.plot(T, R, 'o-')
-plt.xlabel("Temperature (°C)")
-plt.ylabel("Resistance (Ω)")
+plt.xlabel(r"Temperature ($^\circ$C)")
+plt.ylabel("Resistance ($\Omega$)")
 plt.title("Temperature vs Resistance")
 
 plt.grid(True)
